782b63c33767bfc4eb4829630b19ae86bb7a9ccf
badhitman <10000335+badhitman@users.noreply.github.com>
2019-09-02 12:41:37 +0300

    Bootstrap Breadcrumb
    https://getbootstrap.com/docs/4.3/components/breadcrumb/

---
+++ Components/Breadcrumbs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BootstrapViewComponents
+{
+    public class Breadcrumbs : ViewComponent
+    {
+        public class BreadcrumbItem
+        {
+            public string text;
+            public string href;
+        }
+
+        public IViewComponentResult Invoke(List<BreadcrumbItem> BreadcrumbItems)
+        {
+            return View(BreadcrumbItems);
+        }
+    }
+}
