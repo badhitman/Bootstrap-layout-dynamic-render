740afa1854605afd26529a2e21cc9c7a23dcace3
badhitman <10000335+badhitman@users.noreply.github.com>
2019-10-17 20:14:16 +0300

    Update NavBaseViewComponent.cs

--- Components/bootstrap/navs/base/NavBaseViewComponent.cs
+++ Components/bootstrap/navs/base/NavBaseViewComponent.cs
@@ -24,7 +24,7 @@ namespace BootstrapViewComponentsRazorLibrary.Components.bootstrap.nav
         /// <returns></returns>
         public IViewComponentResult Invoke(AbstractNavManager navManager, bool SetPillsTheme)
         {
-            if (navManager.NavigationOrientation == Models.bootstrap.NavOrientationsEnum.HorizontallyFill && navManager.NavWrapperType == Models.NavWrapperTypesEnum.nav)
+            if ((navManager.NavigationOrientation == Models.bootstrap.NavOrientationsEnum.HorizontallyFill || navManager.NavigationOrientation == Models.bootstrap.NavOrientationsEnum.HorizontallyJustified) && navManager.NavWrapperType == Models.NavWrapperTypesEnum.nav)
             {
                 logger.LogInformation("When using a <nav>-based navigation, be sure to include .nav-item on the anchors.");
                 navManager.Childs.ForEach(x => x.AddCSS("nav-item"));
@@ -40,6 +40,12 @@ namespace BootstrapViewComponentsRazorLibrary.Components.bootstrap.nav
             {
                 logger.LogError("Нельзя совмещать Pills и Tabs. Pils автоматически отключён");
                 SetPillsTheme = false;
+            }
+
+            if(navManager.NavigationOrientation == Models.bootstrap.NavOrientationsEnum.HorizontallyFlexUtilities)
+            {
+                logger.LogInformation("Working with flex utilities. If you need responsive nav variations, consider using a series of flexbox utilities.While more verbose, these utilities offer greater customization across responsive breakpoints. In the example below, our nav will be stacked on the lowest breakpoint, then adapt to a horizontal layout that fills the available width starting from the small breakpoint.");
+                navManager.Childs.ForEach(x => x.AddCSS("flex-sm-fill text-sm-center", true));
             }
 
             ViewBag.IsPillsTheme = SetPillsTheme;
