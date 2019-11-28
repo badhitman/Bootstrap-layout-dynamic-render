d9427052ab652ba10f5b1d13889b8d8d9bf11a00
badhitman <10000335+badhitman@users.noreply.github.com>
2019-11-28 10:11:13 +0300

    ~

---
+++ Components/html/hrH.cs
@@ -0,0 +1,16 @@
+﻿////////////////////////////////////////////////
+// https://github.com/badhitman
+////////////////////////////////////////////////
+using BootstrapAspDynamicRender.service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapAspDynamicRender.components
+{
+    public class hrH : ViewComponent
+    {
+        public IViewComponentResult Invoke(hsH SetObjectManager)
+        {
+            return View(SetObjectManager);
+        }
+    }
+}
---
+++ Service/html/hsH.cs
@@ -0,0 +1,37 @@
+﻿using BootstrapAspDynamicRender.components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootstrapAspDynamicRender.service
+{
+    public class hsH : ahsNestedTools
+    {
+        public override string ViewComponentName => nameof(hrH);
+
+        private short _HeaderIndex = 1;
+        public short HeaderIndex
+        {
+            get => _HeaderIndex;
+            set
+            {
+                if (value < 1)
+                {
+                    value = 1;
+                }
+                if (value >6)
+                {
+                    value = 6;
+                }
+                _HeaderIndex = value;
+            }
+        }
+
+        public string Text { get; set; }
+
+        public hsH(short SetHeaderIndex = 1)
+        {
+            HeaderIndex = SetHeaderIndex;
+        }
+    }
+}
